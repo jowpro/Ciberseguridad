5a1587e1c052a5049ec8c8bb0e23da9d5cdd5dbf
angel <el_grow2116@hotmail.com>
2021-10-25 15:22:44 -0400

    version del proyecto

---
+++ js.js
@@ -0,0 +1,12 @@
+
+<a href="">Ques es la seguridad informatica?</a><br><br>
+<a href="">Poque usamos la seguridad informatica?</a><br><br>
+<a href="">Porque surgio la seguridadd informatica?</a><br><br>
+<a href="">Que son los virus?</a><br><br>
+<a href="">Porque existen los virus?</a><br><br>
+<a href="">Que es un hacker?</a><br><br>
+<a href="">Que es hacking etico?</a><br><br>
+<a href="">Diferecia entre hacker y CiberSeguridad</a><br><br>
+<a href="">Los haker son buenos?</a><br><br>
+<a href="">Tipos de hacker</a><br><br>
+<a href="">Tipos de virus</a>
